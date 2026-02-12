88ac5819466d5f5edf779f5aed432d968d7861e0
Shreeney <ajmerishreeney@gmail.com>
2026-02-12 16:20:48 -0500

    add: add full getty logic

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-sed -i '' -e 's|ttyv1.*getty Pc.*|ttyv1 "/usr/libexec/getty Ly" xterm on secure|' /etc/ttys
+cat << 'EOF' >> /etc/gettytab
+Ly:\
+	:lo=/usr/local/bin/ly_wrapper:\
+	:al=root:
+EOF
 
+sed -i '' -e 's|ttyv1.*getty Pc.*|ttyv1 "/usr/libexec/getty Ly" xterm on secure|' /etc/ttys
