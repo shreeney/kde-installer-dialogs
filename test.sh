a88699738245759b17df5d4671a5d027d692f6d4
Shreeney <ajmerishreeney@gmail.com>
2026-02-12 15:51:05 -0500

    add: add new spacing logic for sed

--- test.sh
+++ test.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-
-TARGET_FILE="/etc/ttys"
-OLD_LINE="ttyv1.*/usr/libexec/getty Pc.*"
-NEW_LINE='ttyv1 "/usr/libexec/getty Ly" xterm on secure'
-
-sed -i '' -e "s|$OLD_LINE|$NEW_LINE|" "$TARGET_FILE"
+sed -i '' -e 's|ttyv1.*getty Pc.*|ttyv1 "/usr/libexec/getty Ly" xterm on secure|' /etc/ttys
 
