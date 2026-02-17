8f437efb202eb4186d5e1acc2aaf8a6bc39e553b
Shreeney <ajmerishreeney@gmail.com>
2026-02-17 12:11:37 -0500

    remove test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cat << 'EOF' >> /etc/gettytab
-Ly:\
-	:lo=/usr/local/bin/ly_wrapper:\
-	:al=root:
-EOF
-
-sed -i '' -e 's|ttyv1.*getty Pc.*|ttyv1 "/usr/libexec/getty Ly" xterm on secure|' /etc/ttys
